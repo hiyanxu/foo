0dddab859073d8ccfeb4d3b6117434bd388e328c
闫旭01 <yanxu01@baijia.com>
2021-02-24 20:06:25 +0800

    fix: 0.3.0

--- foo.go
+++ foo.go
@@ -2,5 +2,6 @@ package foo
 
 func Bar() string {
 //	return "this is first version"
-	return "这是一个更好的版本"
+//	return "这是一个更好的版本"
+	return "这是v0.3.0版本"
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module github.com/hiyanxu/foo
 
 go 1.16
+
+// Bar()方法返回值不友好
+retract v0.2.1
