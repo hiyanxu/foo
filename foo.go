457645ed04dd17b6cc0f2c7296592e57a605323d
闫旭01 <yanxu01@baijia.com>
2021-02-24 20:56:31 +0800

    feat: 2.0.0版本

--- foo.go
+++ foo.go
@@ -3,7 +3,8 @@ package foo
 func Bar() string {
 //	return "this is first version"
 //	return "这是一个更好的版本"
-	return "这是v0.3.0版本"
+//	return "这是v0.3.0版本"
+	return "这是v2.0.0版本"
 }
 
 func Quz() string {
